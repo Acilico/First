3a146c3ba74214875900032b2a7385f99bd85e47
Acilico <acilico.su@gmail.com>
2018-10-20 17:13:53 +0300

    My first commit

---
+++ MyMain.java
@@ -0,0 +1,11 @@
+package zadacha;
+
+	import java.util.Scanner;
+
+		public class MyMain {
+			public static void main(String[] args){
+			Scanner scan = new Scanner (System.in);
+			System.out.print("Hello world!");
+		}
+		}
+
